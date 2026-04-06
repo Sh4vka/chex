22b7786b76f7e111a4530d51a04c51dbb86e2a24
Titov Matvey <matvey11334422@gmail.com>
2026-04-06 11:26:46 +0300

    test(cli)

--- test.cpp
+++ test.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/screen/screen.hpp>
 #include "buffer.hpp"
 #include "cursor.hpp"
 #include "view.hpp"
@@ -7,8 +11,22 @@ int main() {
     std::string file = "test.bin";
     core::Buffer buf(file);
     core::Cursor cur(buf.size());
-    buf.insert_byte('f', cur.get_position());
     render::View view;
-    std::cout << view.get_string_hex(buf.get_data(), 0, buf.size()) << std::endl;
+    std::string hex = view.get_string_hex(buf.get_data(), 0, buf.size());
+    std::string src = "";
+    for (size_t i = 0; i < hex.size(); ++i) {
+        src.push_back(hex[i]);
+        i++;
+        src.push_back(hex[i]);
+        src.push_back(' ');
+    }
+    auto data = ftxui::paragraph(src) | ftxui::border;
+    auto infofile = ftxui::text(file) | ftxui::border;
+
+    auto elements = ftxui::vbox({data | ftxui::yflex, infofile});
+    auto render = ftxui::Renderer([&] {return elements;});
+    auto screen = ftxui::ScreenInteractive::Fullscreen();
+    screen.Clear();
+    screen.Loop(render);
     return 0;
 }
