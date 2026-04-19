bf6c82aa7aa7a097fdb644e1a6705f7c2765aabe
Matthew <matvey11334422@gmail.com>
2026-04-19 20:13:21 +0300

    fix(nibble) : Убраны ниблы

--- src/controller.cpp
+++ src/controller.cpp
@@ -6,10 +6,10 @@ namespace render {
     
 void Editor::move_left() {cursor.move_left(1);}
 void Editor::move_right() {cursor.move_right(1);}
-void Editor::move_up() {cursor.move_left(bytes_per_row * 2);}
-void Editor::move_down() {cursor.move_right(bytes_per_row * 2);}
+void Editor::move_up() {cursor.move_left(bytes_per_row);}
+void Editor::move_down() {cursor.move_right(bytes_per_row);}
 
-void Editor::delete_symbol() {buffer.erase_byte(cursor.get_position() * 2);}
+void Editor::delete_symbol() {buffer.erase_byte(cursor.get_position());}
 
 Editor::Editor(core::Buffer &buffer_, core::Cursor &cursor_, size_t bytes_per_row_) : buffer(buffer_), cursor(cursor_), bytes_per_row(bytes_per_row_) {}
 }
